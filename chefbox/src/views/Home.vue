ecf2159c3946d67e1df458d9f704d1f36e6717c1
Glade David Brimhall <56045229+GladeBrimhall@users.noreply.github.com>
2020-05-13 12:38:50 -0600

    Removed helloworld component
    Removed unnessicary code that was installed with Vue

--- chefbox/src/views/Home.vue
+++ chefbox/src/views/Home.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to House Atreides Chefbox." />
     <div class="header">
       <h1>House Atreides Chefbox</h1>
       <img src="../assets/images/HouseA_logo.png">
-      <p>Test</p>
     </div>
   </div>
 </template>
 
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
   }
 };
 </script>
