3bfc8da7c945bee0523ddd073a3c591e5a95b114
Heather L <hmlyman@hotmail.com>
2020-05-26 16:39:50 -0600

    Login/Logout
    Added login/logout button, configured the authorization. Redirects to 400 page not secure. (Working on that)

--- chefbox/src/views/Home.vue
+++ chefbox/src/views/Home.vue
@@ -2,18 +2,27 @@
   <div class="home">
     <div class="header">
       <h1>House Atreides Chefbox</h1>
-      <img src="../assets/images/HouseA_logo.png">
+      <img src="../assets/images/HouseA_logo.png" />
     </div>
   </div>
 </template>
 
 
 <script>
-
-
 export default {
   name: "Home",
-  components: {
+  components: {},
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
   }
 };
 </script>
--- chefbox/src/views/Login.vue
+++ chefbox/src/views/Login.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
+  <div class="login">
     <h1>Login</h1>
+    <div v-if="!$auth.loading">
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "login",
+  components: {},
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+};
+</script>
