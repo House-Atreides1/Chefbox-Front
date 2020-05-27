ac9ad84aaad6db57474aa6c879c257b6e43fc072
Heather L <hmlyman@hotmail.com>
2020-05-27 14:19:37 -0600

    Log in/Log out
    Replaced ID with correct ID. Made dynamic Log In/Log Out buttons and text show up when appropriate. Added correct callbacks in auth0 application settings

--- chefbox/src/views/Login.vue
+++ chefbox/src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login">
-    <h1>Login</h1>
+    <h1 v-if="!$auth.isAuthenticated">Log In</h1>
+    <h1 v-if="$auth.isAuthenticated">Log Out</h1>
     <div v-if="!$auth.loading">
       <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
       <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
