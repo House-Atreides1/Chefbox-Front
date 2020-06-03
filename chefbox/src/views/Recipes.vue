e5a979144e060bd35e026f99f575349191ad27c1
Michael Ball <47047399+mball028@users.noreply.github.com>
2020-06-02 19:51:37 -0600

    recipes page

---
+++ chefbox/src/views/Recipes.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Recipes</h1>
+    <div v-for="recipe in recipes" :key="recipe._id">
+      <h1>{{recipe.title}}</h1>
+      <h3>{{recipe.description}}</h3>
+      <p>{{recipe.directions}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      recipes: []
+    };
+  },
+  methods: {
+    getRecipes() {
+      axios.get("http://localhost:5500/recipes").then(response => {
+        return (this.recipes = response.data);
+      });
+    }
+  },
+  created: function() {
+    this.getRecipes();
+  }
+};
+</script>
